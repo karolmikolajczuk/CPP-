b4dafb40cd77246f6aead4cd2b0b3f24a54dae64
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-04-03 00:19:44 +0200

    Just an amazing code. Nothing more to say :D

--- FactorialHigherThanULong/main.cpp
+++ FactorialHigherThanULong/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdio>
 #include <climits>
+#include <vector>
+#include <string>
 
 /**
  *
@@ -17,7 +19,14 @@
  */
 
 void PrintNumber(char* number) {
-	for(int i=0; i<=19; i++) std::cout << (int)number[i];
+	std::cout << "Printing a number: ";
+	bool flag = true;
+	for(int i=0; i<=19; i++) {
+		if((int)number[i]!=0) flag = false;
+		if(flag && (int)number[i]==0)continue;
+		if(flag && i==19) std::cout << 0;
+		else std::cout << (int)number[i];
+	}
 	std::cout << '\n';
 }
 
@@ -33,69 +42,72 @@ char* ParseNumber(unsigned long* number) {
 }
 
 char* MultiplyingBigNumbers(unsigned long* number1, unsigned long* number2) {
-	char* multiply = new char[128] {0};
-	int index_of_multiply = 127;
-	
-	std::cout << "Parsed" << std::endl;
-
-	std::cout << (*number1) << " " << (*number2) << std::endl;
 	char* number_1 = ParseNumber(number1);
 	char* number_2 = ParseNumber(number2);
-	
-	std::cout << "Printing" << std::endl;
 
 	PrintNumber(number_1);
 	PrintNumber(number_2);
 	
-	// multiplying 
+	char sums_to_add[20][361] {0};
 
-	// multiply index 19 with index 19, 
-	// multiply index 19 with index 18
-	// ..
-	// ..
-	// ..
-	// multiply index 19 with index 0
-	// multiply index 18 with index 19
-	// ..
-	// ..
-	// ..
-	// sum the results
-	//
-	// dont forget to add 0 on the beginnig of multiplying
-	// lower indexes
-	//
-	
-
-	// matrix of sums 19 rows and 361 columns
-	char sums_to_add[19][361];
-	
 	int indexes_of_sums = 360;
 	int index_of_last_row = 360;
 
 	for(int i=19; i>=0; i--) {
-		int add_to_up_next_index = 0;
-		
-		for(int j=19; j>=0; j--) {
-			int down_1 = (int)number2[i], 
-			    up_1 = (int)number1[j];
+		int divider = (int)number_2[i];
+		int number_from_before = 0;
 
-			int result = down_1*up_1;
-			// set 
-			sums_to_add[i][indexes_of_sums]=result%10+add_to_up_next_index;
-					
-			add_to_up_next_index = result/10;
+		for(int j=19; j>=0; j--) {
+			int result = divider * (int)number_1[j];
+			sums_to_add[-i+19][indexes_of_sums]=result%10+number_from_before;	
+			number_from_before = result/10;
+
+			indexes_of_sums--;
 		}
+
+		index_of_last_row--;
+		indexes_of_sums = index_of_last_row;
 	}	
 	
+	char* multiply = new char[361] {0};
+	
+	int add_to_next_column = 0;
+	for(int i=360; i>=0; i--) {
+		int sum_of_column = 0;
+		for(int j=19; j>=0; j--	) {
+			sum_of_column += (int)sums_to_add[j][i];
+		}
+		multiply[i]=sum_of_column%10 + add_to_next_column;
+		add_to_next_column = sum_of_column/10;
+	}
 	return multiply;
 }
 
-int main() {
+void PrintResult(char* result) {
+	std::cout << "Result:" << std::endl;
+	bool dropZero = true;
+	for(int i=0; i<=360; i++) {
+		if((int)result[i] != 0) dropZero = false;
+		if(dropZero) continue;
+		if(dropZero && i==360) std::cout << 0;
+		else std::cout<<(int)result[i];
+	}
+}
 
-	unsigned long a = 1082518925;
-	unsigned long b = 1085151252;
+int main(int argc, char **argv) {
+	
+	if (argc != 3) {
+		std::cerr << "2 arguments";
+	}
+	
+	std::string str_a = std::string(argv[1]);
+	std::string str_b = std::string(argv[2]);
+	unsigned long a = std::strtoul(str_a.c_str(), NULL, 0);
+	unsigned long b = std::strtoul(str_b.c_str(), NULL, 0);
+
 	std::cout << a << " oraz " << b << std::endl;
-	MultiplyingBigNumbers(&a, &b);
+	
+	PrintResult(MultiplyingBigNumbers(&a, &b));
 
 	return 0;
 }
