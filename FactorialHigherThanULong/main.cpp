bde220b94815d564830c4983b238d724e61390be
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-04-02 12:53:06 +0200

    Just realised that my code was working correctly, but didnt notice that i was trying to print chars from range 0-9 where it is not possible to see without int converse.

--- FactorialHigherThanULong/main.cpp
+++ FactorialHigherThanULong/main.cpp
@@ -16,19 +16,48 @@
  *    4 1 8 6
  */
 
+void PrintNumber(char* number) {
+	for(int i=0; i<=19; i++) std::cout << number[i];
+	std::cout << '\n';
+}
+
+char* ParseNumber(unsigned long* number) {
+	char* parsed_number = new char[20] {0};
+	short index = 19;
+	while((*number)>0) {
+		std::cout << "Liczba: " << (*number)%10 << std::endl;
+		parsed_number[index] = (*number)%10;
+		(*number)/=10;
+		index--;
+		std::cout << "Index: " << index << " Parse: " << (int)parsed_number[index+1] << std::endl;
+	}
+	return parsed_number;
+}
 
 char* MultiplyingBigNumbers(unsigned long* number1, unsigned long* number2) {
+	char* multiply = new char[128] {0};
+	
+	std::cout << "Parsed" << std::endl;
 
+	std::cout << (*number1) << " " << (*number2) << std::endl;
+	char* number_1 = ParseNumber(number1);
+	char* number_2 = ParseNumber(number2);
+	
+	std::cout << "Printing" << std::endl;
+
+	PrintNumber(number_1);
+	PrintNumber(number_2);
+		
+	
+	return multiply;
 }
 
 int main() {
-	std::size_t f1 = 1;
-	unsigned long f2 = 1;
-	
-	for (int ind=2; ind <= 500; ind++) {
-		f1*=ind;
-		f2*=ind;
-	}
+
+	unsigned long a = 1082518925;
+	unsigned long b = 1085151252;
+	std::cout << a << " oraz " << b << std::endl;
+	MultiplyingBigNumbers(&a, &b);
 
 	return 0;
 }
